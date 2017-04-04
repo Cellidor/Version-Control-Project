9f981c372d2e0549d479b478bf4425e5490067c4
Samuel Mortimer <mortimsa@sheridancollege.ca>
2017-04-03 23:40:10 -0400

    Release for Alpha build
    Checkpoint functionality has been added to the game. This includes properly respawning the player and playing an audio cue when player has reached the checkpoint in question.

---
+++ Assets/CheckPoint.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CheckPoint : MonoBehaviour {
+
+    public AudioClip ding;
+    AudioSource audioS;
+    private bool gotCheckpoint;
+
+	void Start () {
+        audioS = GetComponent<AudioSource>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player" && !gotCheckpoint)
+        {
+            audioS.PlayOneShot(ding);
+            other.gameObject.SendMessage("SetCheckpoint", gameObject.transform.position);
+            gotCheckpoint = true;
+        }
+    }
+}
