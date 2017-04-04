bb943d5a52c1da9a4ce2a3e5bc2b973517ffd099
Samuel Mortimer <mortimsa@sheridancollege.ca>
2017-04-03 21:45:04 -0400

    Add basic user movement
    Player can now move left and right (A & D), along with jumping with spacebar.

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float jumpForce = 5;
+    public float speed = 2;
+    public Rigidbody body;
+
+	void Start () {
+        body = GetComponent<Rigidbody>();
+	}
+	
+
+	void Update () {
+
+	if (Input.GetKey(KeyCode.D))
+        {
+            body.AddForce(transform.right * speed);
+        }
+    if (Input.GetKey(KeyCode.A))
+        {
+            body.AddForce(-transform.right * speed);
+        }
+    if (Input.GetKeyDown(KeyCode.Space))
+        {
+            body.AddForce(transform.up * jumpForce);
+        }
+	}
+}
