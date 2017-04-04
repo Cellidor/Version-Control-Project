dd4d49545dd9788f4d2a7f0dd716733387aafc75
SHERNET\austruda <austruda@sheridanc.on.ca>
2017-04-03 22:22:24 -0400

    Add basic player health functionality
    script added for use on player prefab. player should have health removed if they contact spikes.

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerHealth : MonoBehaviour {
+
+    public float health;
+    public Vector3 checkPoint;
+	void Start () {
+        checkPoint = gameObject.transform.position; 
+	}
+	
+	void Update () {
+	
+	}
+
+
+    void HealthLoss (int damage)
+    {
+        health -= damage;
+        if (health <= 0)
+        {
+            Respawn();
+        }
+    }
+
+    void SetCheckpoint(Vector3 location)
+    {
+
+    }
+
+    void Respawn()
+    {
+        gameObject.transform.position = checkPoint;
+        health = 100;
+    }
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Spike")
+        {
+            HealthLoss(10);
+        }
+    }
+}
