f545bd1e8e38a8572c8256d69c9f0c4542a2c8eb
Samuel Mortimer <mortimsa@sheridancollege.ca>
2017-04-03 21:56:45 -0400

    Add limitations to PlayerMovement
    Developers now have min and max for players move and jump force, variables hidden, left and right motion tweaked to feel more responsive.

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -3,9 +3,13 @@ using System.Collections;
 
 public class PlayerMovement : MonoBehaviour {
 
+    [Range(20f,40f)]
     public float jumpForce = 5;
+    [Range(1f,5f)]
     public float speed = 2;
-    public Rigidbody body;
+    private Rigidbody body;
+    private string currentMove;
+    private string lastMove;
 
 	void Start () {
         body = GetComponent<Rigidbody>();
@@ -16,10 +20,22 @@ public class PlayerMovement : MonoBehaviour {
 
 	if (Input.GetKey(KeyCode.D))
         {
+            currentMove = "D";
+            if (currentMove != lastMove)
+            {
+                body.velocity = new Vector3 (body.velocity.x/1.5f,body.velocity.y);
+                lastMove = currentMove;
+            }
             body.AddForce(transform.right * speed);
         }
     if (Input.GetKey(KeyCode.A))
         {
+            currentMove = "A";
+            if (currentMove != lastMove)
+            {
+                body.velocity = new Vector3(body.velocity.x / 1.5f, body.velocity.y);
+                lastMove = currentMove;
+            }
             body.AddForce(-transform.right * speed);
         }
     if (Input.GetKeyDown(KeyCode.Space))
